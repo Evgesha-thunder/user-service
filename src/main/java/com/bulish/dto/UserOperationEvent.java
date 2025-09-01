e7d16d72020fcc83d1e4a39e9432ac6f8ff9c11e
Evgenia Bullis <bulish2015@yandex.ru>
2025-09-01 11:10:45 +0300

    add kafka producer for user operations.

--- src/main/java/com/bulish/dto/UserOperationEvent.java
+++ src/main/java/com/bulish/dto/UserOperationEvent.java
@@ -3,11 +3,14 @@ package com.bulish.dto;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
+import java.io.Serializable;
 
 @AllArgsConstructor
+@NoArgsConstructor
 @Builder
 @Data
-public class UserOperationEvent {
+public class UserOperationEvent implements Serializable {
     private UserOperation userOperation;
     private String email;
 }
--- src/main/java/com/bulish/kafka/KafkaUserEventProducer.java
+++ src/main/java/com/bulish/kafka/KafkaUserEventProducer.java
@@ -2,20 +2,17 @@ package com.bulish.kafka;
 
 import com.bulish.dto.UserOperationEvent;
 import com.fasterxml.jackson.core.JsonProcessingException;
-import jakarta.annotation.PreDestroy;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.kafka.clients.producer.*;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.stereotype.Component;
 
-import java.time.Duration;
-
 @Component
 @Slf4j
 @RequiredArgsConstructor
 public class KafkaUserEventProducer {
-    private Producer<String, String> producer;
+    private final Producer<String, String> producer;
     private final ObjectMapper objectMapper;
 
     public void sendEvent(String topic, String key, UserOperationEvent event) {
@@ -32,14 +29,6 @@ public class KafkaUserEventProducer {
             });
         } catch (JsonProcessingException exception) {
             log.error("Error while serialization {}", exception.getMessage());
-        }
-    }
-
-    @PreDestroy
-    public void closeProducer() {
-        if (producer != null) {
-            producer.close(Duration.ofSeconds(5));
-            log.info("Kafka producer closed");
         }
     }
 }
